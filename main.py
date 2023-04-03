961afb3e67d7e0633551cdaa1d111f38d645fdc5
tanvir <mdtanvirt@gmail.com>
2023-04-03 15:45:30 +0600

    Update ui flow

--- main.py
+++ main.py
@@ -3,76 +3,69 @@ import streamlit_survey as ss
 import json
 import pandas as pd
 
-st.set_page_config(layout="wide")
+survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
+#pages = survey.pages(2, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
+pages_test = survey.pages(10, on_submit=lambda: st.success("Your responses have been recorded. Thank you Tanvir!"))
 
-# Hide streamlit default menu and footer from the template
-hide_st_style = """
-    <style>
-    #MainMenu {visibility: hidden}
-    footer {visibility: hidden}
-    header {visibility: hidden}
-    </style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-st.title('Tanvir')
-
-survey = ss.StreamlitSurvey()
-
-pages = survey.pages(3, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
-
-##############
-with pages:
-    if pages.current == 0:
-        st.write("Do you have any car?")
-        used_before = survey.radio(
-            "used_st_before",
-            options=["NA","Yes", "No"],
-            index=0,
-            label_visibility="collapsed",
-            horizontal=True,
+with pages_test:
+    if pages_test.current == 0:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["ALFA", "ROMEO", "ASTON", "MARTIN", "AUDI", "BMW"], horizontal=True, id="1"
+        )
+    if pages_test.current == 1:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["CHEVROLET","DODGE","FERRARI","HONDA"], horizontal=True, id="2"
         )
 
-        if used_before == "Yes":
-            st.write("Pick you fevorit brand")
-            survey.radio(
-                "Select One: ", options = ["ALFA", "ROMEO", "ASTON", "MARTIN", "AUDI", "BMW"], horizontal=True, id="1"
-                )
-            survey.radio(
-                "Select One: ", options = ["CHEVROLET","DODGE","FERRARI","HONDA"], horizontal=True, id="2"
-                )
-            survey.radio(
-                "Select One: ", options = ["JAGUAR","LAMBORGHINI","MAZDA","MCLAREN"], horizontal=True, id="3"
-                )
-            survey.radio(
-                "Select One: ", options = ["MERCEDES-BENZ","NISSAN","PAGANI AUTOMOBILI S.P.A","PORSCHE"], horizontal=True, id="4"
-                )
-            survey.radio(
-                "Select One: ", options = ["FIAT","MINI","SCION","SUBARU"], horizontal=True, id="5"
-                )
-            survey.radio(
-                "Select One: ", options = ["BENTLEY","BUICK","FORD","HYUNDAI"], horizontal=True, id="6"
-                )
-            survey.radio(
-                "Select One: ", options = ["LEXUS","MASERATI","ROUSH","VOLKSWAGEN"], horizontal=True, id="7"
-                )
-            survey.radio(
-                "Select One: ", options = ["ACURA","CADILLAC","INFINITI","KIA"], horizontal=True, id="8"
-                )
-            survey.radio(
-                "Select One: ", options = ["MITSUBISHI" ,"ROLLS-ROYCE","TOYOTA","VOLVO"], horizontal=True, id="9"
-                )
-            survey.radio(
-                "Select One: ", options = ["CHRYSLER","LINCOLN","GMC","RAM"], horizontal=True, id="10"
-                )
-        
-        if used_before == "No":
-            survey.text_area("How you enjoy this survey", placeholder="Type here")
-        
-        if used_before == "NA":
-            survey.text_area("Write something", placeholder="Type here")
-            
-#############
+    if pages_test.current == 2:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["JAGUAR","LAMBORGHINI","MAZDA","MCLAREN"], horizontal=True, id="3"
+        )
+
+    if pages_test.current == 3:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["MERCEDES-BENZ","NISSAN","PAGANI AUTOMOBILI S.P.A","PORSCHE"], horizontal=True, id="4"
+        )
+
+    if pages_test.current == 4:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["FIAT","MINI","SCION","SUBARU"], horizontal=True, id="5"
+        )
+
+    if pages_test.current == 5:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["BENTLEY","BUICK","FORD","HYUNDAI"], horizontal=True, id="6"
+        )
+
+    if pages_test.current == 6:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["LEXUS","MASERATI","ROUSH","VOLKSWAGEN"], horizontal=True, id="7"
+        )
+
+    if pages_test.current == 7:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["ACURA","CADILLAC","INFINITI","KIA"], horizontal=True, id="8"
+        )
+
+    if pages_test.current == 8:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["MITSUBISHI","ROLLS-ROYCE","TOYOTA","VOLVO"], horizontal=True, id="9"
+        )
+
+    if pages_test.current == 9:
+        st.write("test survey")
+        first = survey.radio(
+            "Select One: ", options = ["CHRYSLER","LINCOLN","GMC","RAM"], horizontal=True, id="10"
+        )
 
 json_data = survey.to_json()
 st.json(json_data)
